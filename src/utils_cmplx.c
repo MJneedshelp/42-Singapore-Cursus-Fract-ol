2517df677378d016c2b56a99415b2ac6b385c73f
MJ <mingjuntan93@gmail.com>
2024-08-31 20:22:29 +0800

    added function to find magnitude of complex number

--- src/utils_cmplx.c
+++ src/utils_cmplx.c
@@ -6,13 +6,14 @@
 /*   By: mintan <mintan@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/31 16:30:00 by mintan            #+#    #+#             */
-/*   Updated: 2024/08/31 19:08:04 by mintan           ###   ########.fr       */
+/*   Updated: 2024/08/31 20:19:04 by mintan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/libft.h"
 #include "../include/ft_printf.h"
 #include "../include/get_next_line.h"
+#include <math.h>
 // #include "../include/fractol.h"
 
 typedef struct s_cmplx_number
@@ -22,7 +23,10 @@ typedef struct s_cmplx_number
 }	t_cmplx;
 
 
-
+/* Description: calculates the square of a complex number. Returns a complex
+   number in the form of a structure that contains the real and imaginary
+   parts of the complex number.
+*/
 
 t_cmplx	sqr_cmplx(t_cmplx input)
 {
@@ -34,6 +38,12 @@ t_cmplx	sqr_cmplx(t_cmplx input)
 	return (res);
 }
 
+/* Description: calculates the sum of 2 complex numbers. Sums the real and
+   imaginary parts of the number individually. Returns a complex number in
+   the form of a structure that contains the real and imaginary parts of
+   the complex number.
+*/
+
 t_cmplx	sum_cmplx(t_cmplx input1, t_cmplx input2)
 {
 	t_cmplx	res;
@@ -42,6 +52,31 @@ t_cmplx	sum_cmplx(t_cmplx input1, t_cmplx input2)
 	res.img = input1.img + input2.img;
 	return (res);
 }
+
+
+/* Description: calculates the magnitude of a complex number.
+   - Complex number input: a + bi
+   - Magnitude = sqrt(a^2 + b^2)
+   Return: magnitude of the complex number as a double
+*/
+
+double	magnitude_cmplx(t_cmplx input)
+{
+	double	ret;
+
+	ret = sqrt((input.re * input.re) + (input.img * input.img));
+	return (ret);
+}
+
+
+
+
+/* Description: calculates the results of the mandelbrot equation given the
+   the complex number and the number of iterations.
+   - Mandelbrot equation: Z_n+1 = (Z_n)^2 + C
+   Returns a complex number in the form of a structure that contains the
+   real and imaginary parts of the complex number.
+*/
 
 t_cmplx	calc_mandelbrot(t_cmplx input, int no_iter)
 {
@@ -60,6 +95,8 @@ t_cmplx	calc_mandelbrot(t_cmplx input, int no_iter)
 }
 
 
+
+
 int	main(void)
 {
 	t_cmplx	input1;
@@ -69,7 +106,7 @@ int	main(void)
 	t_cmplx output3;
 	int		no_iter;
 
-	no_iter = 5;
+	no_iter = 3;
 	input1.re = 0.5;
 	input1.img = 0.5;
 	input2.re = 15;
@@ -80,6 +117,7 @@ int	main(void)
 	printf("Square of %f + (%f)i. Output - re: %f | img: %f\n", input1.re, input1.img, output1.re, output1.img);
 	printf("Sum of %f + (%f)i. Output - re: %f | img: %f\n", input1.re, input1.img, output2.re, output2.img);
 	printf("Mandelbrot. C = %f + (%f)i | Iterations: %d | Output - re: %f | img: %f\n", input1.re, input1.img, no_iter, output3.re, output3.img);
+	printf("Mangnitude of madelbrot: %f\n", magnitude_cmplx(output3));
 
 
 	return (0);
